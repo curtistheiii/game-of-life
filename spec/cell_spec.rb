c937fd9b6d865f0e7c5fb536a1f2fd3f7685f1ee
Curtis Baney <curtistheiii@gmail.com>
2018-06-11 14:38:12 -0700

    Has tick method test fails.

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -21,8 +21,8 @@ describe "A cell" do
     expect(cell.neighbors).to be_empty
   end
 
-  xit 'has a tick method' do
-
+  it 'has a tick method' do
+    expect(cell.tick)
   end
 
   describe 'after a tick' do
