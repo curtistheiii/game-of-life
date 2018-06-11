e43d4cdcb0d9efc9fb06c4ac0f65895992376841
Curtis Baney <curtistheiii@gmail.com>
2018-06-11 14:36:42 -0700

    Neighbor cells array test starts empty.

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,12 +1,14 @@
 class Cell
   attr_accessor :alive
+  attr_accessor :neighbors
 
   def initialize
     @alive = true
+    @neighbors = []
   end
 
   def kill
     @alive = false
   end
-  
+
 end
